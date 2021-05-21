b51d5ba1ec41577cf8350c0fa6d0f6add643e65b
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-05-21 17:29:32 -0300

    Criando o modelo de dominio

---
+++ LanchesMac/Models/Lanche.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesMac.Models
+{
+    public class Lanche
+    {
+        public int LanchId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemgeUrl { get; set; }
+        public string ImagemThuambnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
